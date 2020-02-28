721a68bf237e6810362347d7b068060f12a3fc81
David Squire <dsquire@vbainfo.com>
2020-02-27 19:49:03 -0600

    Updated to remove Vision Components

--- drone.py
+++ drone.py
@@ -1,11 +1,10 @@
 from pyparrot.Minidrone import Mambo
-from pyparrot.DroneVision import DroneVision
-from pyparrot.Model import Model
 import threading
-import cv2
 import time
-
-mambo = Mambo(mamboAddr, use_wifi=True)
+red="e0:14:19:8e:3d:cf"
+blue="d0:3a:27:16:e6:3b"
+mamboAddr=blue
+mambo = Mambo(mamboAddr, use_wifi=False)
 print("trying to connect")
 success = mambo.connect(num_retries=3)
 print("connected: %s" % success)
@@ -13,28 +12,29 @@ print("connected: %s" % success)
 if (success):
     mambo.ask_for_state_update()
     mambo.smart_sleep(2)
-    print("Preparing to open vision")
-    mamboVision = DroneVision(mambo, Model.MAMBO, buffer_size=30)
-    userVision = UserVision(mamboVision)
-    mamboVision.set_user_callback_function(userVision.save_pictures, user_callback_args=None)
-    success = mamboVision.open_video()
-    print("Success in opening vision is %s" % success)
-
     print("taking off!")
-    mambo.safe_takeoff(5)
-    print("shoot the gun")
-    mambo.fire_gun()
+    mambo.safe_takeoff(2)
 
+    print("shoot the gun")
+    for shots in range(3):
+       print("Fire: " + str(shots))
+       mambo.fire_gun()
+       mambo.smart_sleep(1)
+
+    mambo.smart_sleep(2)
     print("Flying direct: yaw")
-    mambo.fly_direct(roll=0, pitch=0, yaw=50, vertical_movement=0, duration=1)
+    mambo.fly_direct(roll=0, pitch=50, yaw=0, vertical_movement=0, duration=1)
+
+    print("Flying direct: going around in a circle (yes you can mix roll, pitch, yaw in one command!)")
+    mambo.fly_direct(roll=25, pitch=0, yaw=50, vertical_movement=0, duration=3)
 
     print("Flying direct: going backwards (negative pitch)")
     mambo.fly_direct(roll=0, pitch=-50, yaw=0, vertical_movement=0, duration=0.5)
 
-    print("Ending the sleep and vision")
-    mamboVision.close_video()
+    mambo.smart_sleep(2)
+    print("landing")
+    mambo.safe_land(5)
 
-    mambo.smart_sleep(5)
 
     print("disconnecting")
     mambo.disconnect()
